19f7a74316fa37b05c58fa346112f35bfbaf0dd8
Develing1991 <completed0728@gmail.com>
2024-07-11 13:44:55 +0900

    style: add default font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {},
+        fontSize: {
+            xs: [
+                '0.75rem',
+                {
+                    lineHeight: '1rem',
+                    // letterSpacing: '-0.01em',
+                    // fontWeight: '700',
+                },
+            ],
+            // '3xl': [
+            //     '1.875rem',
+            //     {
+            //         lineHeight: '2.25rem',
+            //         letterSpacing: '-0.02em',
+            //         fontWeight: '700',
+            //     },
+            // ],
+        },
     },
     plugins: [],
 }
